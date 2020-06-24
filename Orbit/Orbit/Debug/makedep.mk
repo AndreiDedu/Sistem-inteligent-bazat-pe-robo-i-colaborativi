73c1a80edaf289aeca2f86fbb53ed601e995aabd
AndreiDedu <67372709+AndreiDedu@users.noreply.github.com>
2020-06-24 19:38:52 +0300

    Add files via upload

---
+++ Orbit/Orbit/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+kilolib\kilolib.c
+
+kilolib\message_crc.c
+
+kilolib\message_send.S
+
+main.c
+
